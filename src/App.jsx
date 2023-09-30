cd9e8809ca7f22b397b653022c3aff88ba5e8cc0
Anupama_Halder <anupamahalder2022@gmail.com>
2023-09-30 13:29:30 +0530

    Install Tailwind css and added the heading

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center items-center">
+        <h1 className='text-5xl font-bold my-10 text-gray-800'>Simple Timer</h1>
+      </div>
+    </>
+  )
+}
+ 
+export default App
