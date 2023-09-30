a86cddcfcbdf4a5f20a72f8040ff6afac6df72f1
Anupama_Halder <anupamahalder2022@gmail.com>
2023-09-30 13:42:09 +0530

    Style two button of increment and decrement button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-function App() {
+import { useState } from "react"
 
+function App() {
+  // declare a state to hold the value of count 
+  const [count, setCount] = useState(0);
   return (
     <>
       <div className="flex justify-center items-center">
         <h1 className='text-5xl font-bold my-10 text-gray-800'>Simple Timer</h1>
+      </div>
+      <div className="flex justify-center items-center">
+        <div className="">
+          <h1 className="text-4xl pl-16 font-semibold mb-6">{count}</h1>
+          <div className="flex ">
+          <button 
+          className="py-2 px-4 bg-gray-400 rounded-md mr-12 text-2xl"> + </button>
+          <button 
+          className="py-1 px-5 bg-gray-400 rounded-md text-3xl"> - </button>
+          </div>
+        </div>
       </div>
     </>
   )
